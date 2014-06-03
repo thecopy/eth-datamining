60fcf63421f06eb63e6933fe45aa5a9cddcc9817
Erik Jonsson Thorén <thecopy@gmail.com>
2014-06-03 15:37:49 +0200

    mapper and reducer

--- 03_K-Means/code/mapper.py
+++ 03_K-Means/code/mapper.py
@@ -1,77 +1,20 @@
 #!/usr/bin/env python2.7
+
 import sys
 import numpy as np
+from sklearn.cluster import MiniBatchKMeans
 
-def dist(x,y):
-	return np.linalg.norm(x-y)
-
-def calculateCoreset_naive(data,k):
-    indices = np.random.randint(low = 0, high = len(data), size = k)
-    return data[indices]
-
-def calculateCoreset_adative(data,k):
-	dim = data.shape[1]
-	e = 0.5
-	delta = 0.9999
-
-	print "Calculating Coreset"
-	print "             Method: Adaptive Sampling"
-	print "   Size of data-set: " + str(data.shape)
-	print "                  k: " + str(k)
-	print "                  d: " + str(dim)
-	print "                  e: " + str(e)
-	print "              delta: " + str(delta)
-	print "             len(D): " + str(len(data))
-	print "           decision: " + str(int(10 * dim * k * np.log(1 / delta)))
-	print "  ------------------------------"
-
-	B = np.empty(shape=[0, 750], dtype=float)
-	D = data # D'
-
-	while( len(D) > 10 * dim * k * np.log(1 / delta) ):
-		# Get S
-		b = int( 10 * dim * k * np.log(1 / delta) )
-		s_indices = np.random.randint(low = 0, high = len(D), size = b)
-		S = D[s_indices];
-
-		# Remove |D/2| points from D which are the closest to S
-		distances = []
-		for i, D_point in enumerate(D):
-			for S_point in S:
-				distances.append([ dist(S_point,D_point), i])
-
-		pointsToDelete = []
-		for i, point in enumerate(sorted(distances, key=lambda p: p[0])):
-			if(i >= len(D)/2):
-				break
-			pointsToDelete.append(point[1])
-
-		print "                  b: " + str(b)
-		print "   Points to delete: " + str(len(pointsToDelete))
-		return "a"
-
-def readInput():
- 	# Remove this for production. Uses a local file so i can run from Sublime
-	# return only_dev_read_data_from_file();
-
-	ret = []
-	for line in sys.stdin:
-		ret.append(map(float,line.split(" ")))
-	return np.array(ret);
-
-def printResult(coreset):
-	for line in coreset:
-		print(" ".join(map(str, line)))
-
-
-# Used for dev. 
-def only_dev_read_data_from_file():
-	data = np.load("../data/tiny_subset")['arr_0']
-	return data;
+def emit(key, value):
+    print('%s\t%s' % (key, value))
 
 if __name__ == "__main__":
-	data = readInput()
-	k = 200
-	coreset = calculateCoreset_naive(data ,k)
-	printResult(coreset)
-
+    X = np.zeros(shape=(0, 750))
+    for line in sys.stdin:
+        sample = np.fromstring(line, sep=" ")
+        
+        X = np.vstack( (X, sample) )
+    
+    mbk = MiniBatchKMeans(n_clusters=200, init='k-means++', n_init=5, batch_size=150)
+    mbk.fit(X)
+    
+    emit(1, mbk.cluster_centers_.tolist())
--- 03_K-Means/code/reducer.py
+++ 03_K-Means/code/reducer.py
@@ -1,50 +1,40 @@
 #!/usr/bin/env python2.7
-import sys
+
 import numpy as np
+import sys
+from ast import literal_eval
 
-def merge_and_compress(left,right):
-	k = 200
-	merged = left + right;
+values = None
+N = 0
 
-	return compress_naive(merged,k)
-
-def compress_naive(coreset,k):
-	indices = np.random.randint(low = 0, high = len(coreset), size = k)
-	return np.array(coreset)[indices]
+#def listify(row):
+#    return ' '.join(map(str, row.tolist()))
 
 if __name__ == "__main__":
-	
-	current = [] # we will keep filling this up
-	last = [] # this is the last coreset we filled up
-	merged = None # the merged and compressed coreset (of current and last)
-
-	k = 200
-	i = 0
-	for line in sys.stdin:
-		data = map(float, line.split(" "))
-		current.append(data)
-
-		i+=1 # number of data-points collected
-
-		if(i == k): # We are finished with the current coreset
-
-			if(len(last) > 0): # We have collected two coresets; merge them
-				_merged = merge_and_compress(current,last) # this is our compressed set to the right 
-
-				if merged is None: # This is the exception, only happens the first time, move it to the left
-					merged = _merged
-				else: # We have a coreset to the left, so we merge them
-					merged = merge_and_compress(_merged, merged)
-
-				last = []
-			else: # Or else, we only have one complete coreset; set current to the last and begin collecting another
-				last = current
-				current = []
-			i = 0
-
-	if(merged is None):
-		merged = last
-
-	# No more coresets, thus merged is the final coreset, so we print it
-	for point in merged:
-		print " ".join(map(str, point))
+    for line in sys.stdin:
+        line = line.strip()
+        key, value = line.split('\t')
+        value = np.asmatrix(literal_eval(value))
+        
+        if values is None:
+            values = value
+        else:
+            values += value
+        
+        N += 1
+    
+    values = values / float(N)
+    
+    i = 0
+    values_list = values.tolist()
+    while i < len(values_list):
+        j = 0
+        while j < len(values_list[i]):
+            if j != len(values_list[i]) -1:
+                sys.stdout.write(str(values_list[i][j]) + ' ')
+            else:
+                sys.stdout.write(str(values_list[i][j]) + '\n')
+            
+            j += 1
+        i += 1 
+#    print '\n'.join(np.apply_along_axis( listify, axis=1, arr=values / float(N) ) )
