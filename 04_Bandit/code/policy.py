e8c3987e32d418ac0e6f8fd0172fd36b9ebc292f
ruiixu23 <ruifeng.x.cn@gmail.com>
2014-06-03 14:31:04 +0200

    bug fixing

--- 04_Bandit/code/policy.py
+++ 04_Bandit/code/policy.py
@@ -10,58 +10,63 @@ A_inverse = dict()
 b = dict()
 d = 6
 alpha = 0.2
-chosen = 0
+chosen_article = 0
+chosen_feature = []
 
 # Evaluator will call this function and pass the article features.
 # Check evaluator.py description for details.
 def set_articles(articles):
+    global article_features, A, A_inverse, b, d, alpha, chosen_article, chosen_feature
+
     for article in articles:
-        article_features[article] = np.array(articles[article])
+        article_features[article] = articles[article]
 
 # This function will be called by the evaluator.
 # Check task description for details.
 def update(reward):
+    global article_features, A, A_inverse, b, d, alpha, chosen_article, chosen_feature
+
     if reward == -1:
         return
 
-    article_A = A[chosen]
-    article_b = b[chosen]
-    article_feature = article_features[chosen]
-    A[chosen] = article_A + np.dot(article_feature, article_feature.T)
-    b[chosen] = article_b + reward * article_feature
-    A_inverse[chosen] = np.linalg.inv(A[chosen])
+    A[chosen_article] += np.outer(chosen_feature, chosen_feature)
+    b[chosen_article] += reward * chosen_feature
+    A_inverse[chosen_article] = np.linalg.inv(A[chosen_article])
 
 # This function will be called by the evaluator.
 # Check task description for details.
-def reccomend(timestamp, user_features, articles):
+def reccomend(timestamp, user_feature, articles):
+    global article_features, A, A_inverse, b, d, alpha, chosen_article, chosen_feature
+
     p = []
     article_ids = []
+
     for article in articles:
         if article not in A:
             A[article] = np.identity(d)
-            A_inverse[article] = np.linalg.inv(np.identity(d))
+            A_inverse[article] = np.identity(d)
             b[article] = np.zeros(d)
 
         article_A_inverse = A_inverse[article]
         article_b = b[article]
-        article_feature = article_features[article]
+        feature = np.array(user_feature)
 
         article_theta = np.dot(article_A_inverse, article_b)
-        article_p = np.dot(article_theta.T, article_feature)
-        + alpha * math.sqrt(
-            np.dot(
+        article_p = np.dot(article_theta, feature) + \
+            alpha * math.sqrt(
                 np.dot(
-                    article_feature.T,
-                    article_A_inverse
-                ),
-                article_feature
+                    np.dot(
+                        feature,
+                        article_A_inverse
+                    ),
+                    feature
+                )
             )
-        )
-        article_ids.append(article)
         p.append(article_p)
-
-    global chosen
+        article_ids.append(article)
 
     max_p_indices = [i for i, j in enumerate(p) if j == max(p)]
-    chosen = article_ids[random.randint(0, len(max_p_indices) - 1)]
-    return chosen
+    # chosen_article = article_ids[random.randint(0, len(max_p_indices) - 1)]
+    chosen_article = article_ids[np.argmax(p)]
+    chosen_feature = np.array(user_feature)
+    return chosen_article
